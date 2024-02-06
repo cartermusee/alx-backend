bb3e0734cc2c109c6375498829a951ff900e54dc
cartermusee <cartermusee@gmail.com>
2024-02-06 16:59:01 +0300

    Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+"""module for flask app"""
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
+    """rendering the templates"""
     return render_template("index.html")
 
 if __name__ == '__main__':
