28253b27056d55f8c21ff768bab696095eb05ea2
cartermusee <cartermusee@gmail.com>
2024-02-06 17:17:09 +0300

    Flask app and babel with get_locale function

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,9 +18,12 @@ babel = Babel(app)
 
 app.config.from_object(Config)
 
+
 @babel.localeselector
 def get_locale():
+    """get locale func to get the language"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route("/")
 def index():
