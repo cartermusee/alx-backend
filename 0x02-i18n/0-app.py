fa0809dabd887d9e13f3ebfd623e23704b4d7a5b
cartermusee <cartermusee@gmail.com>
2024-02-06 17:01:02 +0300

    Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """rendering the templates"""
-    return render_template("index.html")
+    return render_template("0-index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
