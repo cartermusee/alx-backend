66403c5e9c4ec8d6c601b2baee2f3056df983b8c
cartermusee <cartermusee@gmail.com>
2024-02-06 17:09:13 +0300

    Flask app and babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""module for flask app and initraing a babel"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+babel = Babel(app)
+
+app.config.from_object(Config)
+
+@app.route("/")
+def index():
+    """rendering the templates"""
+    return render_template("0-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
