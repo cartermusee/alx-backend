c24062cabe93e6d838959201e0a9e06b84a977db
cartermusee <cartermusee@gmail.com>
2024-02-06 17:24:59 +0300

    Flask app and babel with get_locale function rendering

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""module for flask app and initraing a babel"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """class for babels configs"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+babel = Babel(app)
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """get locale func to get the language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """rendering the templates"""
+    return render_template("0-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
